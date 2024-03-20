dc7a49ccd3762594bc021f73f2956f62307ef684
Bedoya0930 <163351947+Bedoya0930@users.noreply.github.com>
2024-03-20 15:10:18 -0500

    Delete ProyectoFinalBasesdeDatos directory

--- ProyectoFinalBasesdeDatos/Views/View_2.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW VistaTotalVentasPorProducto AS
-SELECT p.Nombre AS NombreProducto, SUM(v.Total) AS TotalVentas
-FROM Productos p
-JOIN Ventas v ON p.ID = v.ProductoID
-GROUP BY p.Nombre
-ORDER BY TotalVentas DESC;
-
-SELECT * FROM VistaTotalVentasPorProducto;
--- ProyectoFinalBasesdeDatos/Views/View_3.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW VistaCodigoVentaNombreProducto AS
-SELECT v.ID AS CodigoVenta, CONCAT(p.Nombre, ' - ', v.Fecha) AS NombreProductoFechaVenta
-FROM Productos p
-JOIN Ventas v ON p.ID = v.ProductoID;
-
-
-SELECT * FROM VistaCodigoVentaNombreProducto;
